45aa47a4916805ccb5df556d92db63586f918c6c
Andrew Walbran <qwandor@google.com>
2024-07-15 16:38:16 +0100

    Skeleton for QEMU based on Comprehensive Rust example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+mod exceptions;
+
+use core::panic::PanicInfo;
+use log::error;
+use smccc::{psci::system_off, Hvc};
+
+#[no_mangle]
+extern "C" fn main() {
+    system_off::<Hvc>().unwrap();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    error!("{info}");
+    loop {}
 }
