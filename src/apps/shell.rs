da7b1348b6277c5f81bc0709862a13774f77f0d9
Andrew Walbran <qwandor@google.com>
2024-07-16 15:45:33 +0100

    Treat EOF as exit, and ignore other control characters.

--- src/apps/shell.rs
+++ src/apps/shell.rs
@@ -2,12 +2,14 @@ use core::fmt::Write;
 use embedded_io::Read;
 use tinyvec::{array_vec, ArrayVec};
 
+const EOF: u8 = 0x04;
+
 pub fn main(console: &mut (impl Write + Read)) {
     loop {
         write!(console, "$ ").unwrap();
         let line = read_line(console);
         match line.as_ref() {
-            b"exit" => break,
+            b"exit" | [EOF] => break,
             _ => {
                 writeln!(console, "Unrecognised command.").unwrap();
             }
@@ -20,13 +22,22 @@ fn read_line(console: &mut (impl Write + Read)) -> ArrayVec<[u8; 128]> {
     loop {
         let mut c = [0];
         console.read_exact(&mut c).unwrap();
-        let c = c[0];
-        if c == b'\r' {
-            console.write_str("\r\n").unwrap();
-            return line;
-        } else {
-            console.write_char(c.into()).unwrap();
-            line.push(c);
+        match c[0] {
+            b'\r' => {
+                console.write_str("\r\n").unwrap();
+                return line;
+            }
+            EOF if line.is_empty() => {
+                console.write_str("\r\n").unwrap();
+                line.push(EOF);
+                return line;
+            }
+            c => {
+                if !c.is_ascii_control() {
+                    console.write_char(c.into()).unwrap();
+                    line.push(c);
+                }
+            }
         }
     }
 }
