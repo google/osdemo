20fff9f18ec3607ba1b9666cbf86e8aeeb1512d3
Andrew Walbran <qwandor@google.com>
2024-07-22 14:50:06 +0100

    Guard shared console spinlock with PerCore.
    This will prevent deadlock if an exception handler tries to take the
    spinlock to write to the console.

--- src/console.rs
+++ src/console.rs
@@ -1,13 +1,14 @@
 use crate::platform::{ConsoleImpl, Platform, PlatformImpl};
 use core::{convert::Infallible, fmt, panic::PanicInfo};
 use embedded_io::{ErrorType, Read, Write};
+use percore::{exception_free, ExceptionLock};
 use spin::{mutex::SpinMutex, Once};
 
 static CONSOLE: Once<SharedConsole<ConsoleImpl>> = Once::new();
 
 /// A console guarded by a spin mutex so that it may be shared between threads.
 pub struct SharedConsole<T: Send> {
-    console: SpinMutex<T>,
+    console: ExceptionLock<SpinMutex<T>>,
 }
 
 impl<T: Send> ErrorType for &SharedConsole<T> {
@@ -16,30 +17,30 @@ impl<T: Send> ErrorType for &SharedConsole<T> {
 
 impl<T: Send + fmt::Write> fmt::Write for &SharedConsole<T> {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.console.lock().write_str(s)
+        exception_free(|token| self.console.borrow(token).lock().write_str(s))
     }
 }
 
 impl<T: Send + ErrorType<Error = Self::Error> + Write> Write for &SharedConsole<T> {
     fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
-        self.console.lock().write(buf)
+        exception_free(|token| self.console.borrow(token).lock().write(buf))
     }
 
     fn flush(&mut self) -> Result<(), Self::Error> {
-        self.console.lock().flush()
+        exception_free(|token| self.console.borrow(token).lock().flush())
     }
 }
 
 impl<T: Send + ErrorType<Error = Self::Error> + Read> Read for &SharedConsole<T> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
-        self.console.lock().read(buf)
+        exception_free(|token| self.console.borrow(token).lock().read(buf))
     }
 }
 
 /// Initialises the shared console.
 pub fn init(console: ConsoleImpl) -> &'static SharedConsole<ConsoleImpl> {
     let shared = CONSOLE.call_once(|| SharedConsole {
-        console: SpinMutex::new(console),
+        console: ExceptionLock::new(SpinMutex::new(console)),
     });
 
     shared
@@ -48,8 +49,10 @@ pub fn init(console: ConsoleImpl) -> &'static SharedConsole<ConsoleImpl> {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(console) = CONSOLE.get() {
-        // Ignore any errors writing to the console, to avoid panicking recursively.
-        let _ = writeln!(console.console.lock(), "{}", info);
+        exception_free(|token| {
+            // Ignore any errors writing to the console, to avoid panicking recursively.
+            let _ = writeln!(console.console.borrow(token).lock(), "{}", info);
+        });
     }
     PlatformImpl::power_off();
 }
