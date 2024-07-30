7e3b1f5a60bf5a96509c8145533b93f8db4e7e6e
Andrew Walbran <qwandor@google.com>
2024-07-30 15:13:40 +0100

    Add generic VirtIO transport.
    This will allow abstracting over MMIO vs. PCI devices.

--- src/devices.rs
+++ src/devices.rs
@@ -1,13 +1,12 @@
-use crate::virtio::VirtioHal;
+use crate::virtio::{SomeTransport, VirtioHal};
 use alloc::vec::Vec;
-use virtio_drivers::{
-    device::{blk::VirtIOBlk, console::VirtIOConsole, socket::VsockConnectionManager},
-    transport::mmio::MmioTransport,
+use virtio_drivers::device::{
+    blk::VirtIOBlk, console::VirtIOConsole, socket::VsockConnectionManager,
 };
 
 #[derive(Default)]
 pub struct Devices {
-    pub block: Vec<VirtIOBlk<VirtioHal, MmioTransport>>,
-    pub console: Vec<VirtIOConsole<VirtioHal, MmioTransport>>,
-    pub vsock: Vec<VsockConnectionManager<VirtioHal, MmioTransport>>,
+    pub block: Vec<VirtIOBlk<VirtioHal, SomeTransport>>,
+    pub console: Vec<VirtIOConsole<VirtioHal, SomeTransport>>,
+    pub vsock: Vec<VsockConnectionManager<VirtioHal, SomeTransport>>,
 }
--- src/virtio.rs
+++ src/virtio.rs
@@ -11,7 +11,8 @@ use virtio_drivers::{
     },
     transport::{
         mmio::{MmioError, MmioTransport, VirtIOHeader},
-        DeviceType, Transport,
+        pci::PciTransport,
+        DeviceStatus, DeviceType, Transport,
     },
     BufferDirection, Hal, PhysAddr, PAGE_SIZE,
 };
@@ -49,7 +50,7 @@ pub fn find_virtio_mmio_devices(fdt: &Fdt, devices: &mut Devices) {
                                 transport.version(),
                                 transport.read_device_features(),
                             );
-                            init_virtio_device(transport, devices);
+                            init_virtio_device(transport.into(), devices);
                         }
                     }
                 }
@@ -60,7 +61,7 @@ pub fn find_virtio_mmio_devices(fdt: &Fdt, devices: &mut Devices) {
     }
 }
 
-fn init_virtio_device(transport: MmioTransport, devices: &mut Devices) {
+fn init_virtio_device(transport: SomeTransport, devices: &mut Devices) {
     match transport.device_type() {
         DeviceType::Block => {
             devices.block.push(VirtIOBlk::new(transport).unwrap());
@@ -125,4 +126,130 @@ unsafe impl Hal for VirtioHal {
 
 fn virt_to_phys(vaddr: usize) -> PhysAddr {
     vaddr
+}
+
+/// A wrapper for an arbitrary VirtIO transport, either MMIO or PCI.
+#[derive(Debug)]
+pub enum SomeTransport {
+    Mmio(MmioTransport),
+    Pci(PciTransport),
+}
+
+impl From<MmioTransport> for SomeTransport {
+    fn from(mmio: MmioTransport) -> Self {
+        Self::Mmio(mmio)
+    }
+}
+
+impl From<PciTransport> for SomeTransport {
+    fn from(pci: PciTransport) -> Self {
+        Self::Pci(pci)
+    }
+}
+
+impl Transport for SomeTransport {
+    fn device_type(&self) -> DeviceType {
+        match self {
+            Self::Mmio(mmio) => mmio.device_type(),
+            Self::Pci(pci) => pci.device_type(),
+        }
+    }
+
+    fn read_device_features(&mut self) -> u64 {
+        match self {
+            Self::Mmio(mmio) => mmio.read_device_features(),
+            Self::Pci(pci) => pci.read_device_features(),
+        }
+    }
+
+    fn write_driver_features(&mut self, driver_features: u64) {
+        match self {
+            Self::Mmio(mmio) => mmio.write_driver_features(driver_features),
+            Self::Pci(pci) => pci.write_driver_features(driver_features),
+        }
+    }
+
+    fn max_queue_size(&mut self, queue: u16) -> u32 {
+        match self {
+            Self::Mmio(mmio) => mmio.max_queue_size(queue),
+            Self::Pci(pci) => pci.max_queue_size(queue),
+        }
+    }
+
+    fn notify(&mut self, queue: u16) {
+        match self {
+            Self::Mmio(mmio) => mmio.notify(queue),
+            Self::Pci(pci) => pci.notify(queue),
+        }
+    }
+
+    fn get_status(&self) -> DeviceStatus {
+        match self {
+            Self::Mmio(mmio) => mmio.get_status(),
+            Self::Pci(pci) => pci.get_status(),
+        }
+    }
+
+    fn set_status(&mut self, status: DeviceStatus) {
+        match self {
+            Self::Mmio(mmio) => mmio.set_status(status),
+            Self::Pci(pci) => pci.set_status(status),
+        }
+    }
+
+    fn set_guest_page_size(&mut self, guest_page_size: u32) {
+        match self {
+            Self::Mmio(mmio) => mmio.set_guest_page_size(guest_page_size),
+            Self::Pci(pci) => pci.set_guest_page_size(guest_page_size),
+        }
+    }
+
+    fn requires_legacy_layout(&self) -> bool {
+        match self {
+            Self::Mmio(mmio) => mmio.requires_legacy_layout(),
+            Self::Pci(pci) => pci.requires_legacy_layout(),
+        }
+    }
+
+    fn queue_set(
+        &mut self,
+        queue: u16,
+        size: u32,
+        descriptors: PhysAddr,
+        driver_area: PhysAddr,
+        device_area: PhysAddr,
+    ) {
+        match self {
+            Self::Mmio(mmio) => mmio.queue_set(queue, size, descriptors, driver_area, device_area),
+            Self::Pci(pci) => pci.queue_set(queue, size, descriptors, driver_area, device_area),
+        }
+    }
+
+    fn queue_unset(&mut self, queue: u16) {
+        match self {
+            Self::Mmio(mmio) => mmio.queue_unset(queue),
+            Self::Pci(pci) => pci.queue_unset(queue),
+        }
+    }
+
+    fn queue_used(&mut self, queue: u16) -> bool {
+        match self {
+            Self::Mmio(mmio) => mmio.queue_used(queue),
+            Self::Pci(pci) => pci.queue_used(queue),
+        }
+    }
+
+    fn ack_interrupt(&mut self) -> bool {
+        match self {
+            Self::Mmio(mmio) => mmio.ack_interrupt(),
+            Self::Pci(pci) => pci.ack_interrupt(),
+        }
+    }
+
+    fn config_space<T: 'static>(&self) -> virtio_drivers::Result<NonNull<T>> {
+        match self {
+            Self::Mmio(mmio) => mmio.config_space(),
+            Self::Pci(pci) => pci.config_space(),
+        }
+    }
 }
