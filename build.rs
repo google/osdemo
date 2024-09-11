e530043d99a311792ba613024a77f3455af93e13
Andrew Walbran <qwandor@google.com>
2024-09-11 14:40:42 +0100

    Reduce duplication in build script.

--- build.rs
+++ build.rs
@@ -15,33 +15,30 @@
 use cc::Build;
 use std::env;
 
+const PLATFORMS: [&str; 2] = ["crosvm", "qemu"];
+
 fn main() {
-    println!("cargo::rustc-check-cfg=cfg(platform, values(\"crosvm\", \"qemu\"))");
+    println!(
+        "cargo::rustc-check-cfg=cfg(platform, values(\"{}\"))",
+        PLATFORMS.join("\", \"")
+    );
 
     env::set_var("CROSS_COMPILE", "aarch64-none-elf");
     env::set_var("CC", "clang");
 
     let platform = env::var("CARGO_CFG_PLATFORM").expect("Missing platform name");
-    match platform.as_ref() {
-        "qemu" => {
-            Build::new()
-                .file("asm/entry.S")
-                .file("asm/exceptions.S")
-                .file("asm/idmap_qemu.S")
-                .compile("empty");
-            println!("cargo:rustc-link-arg=-Tlinker/qemu.ld");
-        }
-        "crosvm" => {
-            Build::new()
-                .file("asm/entry.S")
-                .file("asm/exceptions.S")
-                .file("asm/idmap_crosvm.S")
-                .compile("empty");
-            println!("cargo:rustc-link-arg=-Tlinker/crosvm.ld");
-        }
-        _ => {
-            panic!("Unexpected platform name \"{}\"", platform);
-        }
-    }
+    assert!(
+        PLATFORMS.contains(&platform.as_str()),
+        "Unexpected platform name {:?}. Supported platforms: {:?}",
+        platform,
+        PLATFORMS,
+    );
+
+    Build::new()
+        .file("asm/entry.S")
+        .file("asm/exceptions.S")
+        .file(format!("asm/idmap_{}.S", platform))
+        .compile("empty");
+    println!("cargo:rustc-link-arg=-Tlinker/{platform}.ld");
     println!("cargo:rustc-link-arg=-Tlinker/image.ld");
 }
